06be3026f3fbf5a60d7d3b7748943d957e2af166
Brayan <brayan_stiv96@hotmail.com>
2022-10-20 04:32:09 -0500

    Se hacen todos los botones y se logra actualizar la comision, falta sumarla

--- app/src/main/java/com/example/myshop/MainActivity.java
+++ app/src/main/java/com/example/myshop/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.myshop;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    FirebaseFirestore myStore = FirebaseFirestore.getInstance();
+    String idSeller;
+    String totalcomision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +41,192 @@ public class MainActivity extends AppCompatActivity {
         EditText name = findViewById(R.id.etname);
         EditText email = findViewById(R.id.etemail);
         EditText phone = findViewById(R.id.etphone);
-        EditText comision = findViewById(R.id.etcommision);
+        TextView comision = findViewById(R.id.etcommision);
+        Button salesView = findViewById(R.id.btnsales);
         Button add = findViewById(R.id.btnadd);
         Button search= findViewById(R.id.btnsearch);
         Button update = findViewById(R.id.btnupdate);
         Button delete = findViewById(R.id.btndelete);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+       comision.setText(getIntent().getStringExtra("comision"));
+
+        salesView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),sale.class));
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSeller(name.getText().toString(),email.getText().toString(),phone.getText().toString());
+
+            }
+        });
+
+
+//
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myStore.collection("seller")
+                        .whereEqualTo("email", email.getText().toString())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (!task.getResult().isEmpty()) {
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            idSeller = document.getId();
+                                            name.setText(document.getString("name"));
+                                            phone.setText(document.getString("phone"));
+                                            comision.setText(document.getString("comision"));
+
+
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(),"Este cliente no se encuentra en nuestra base de datos",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
+            }
+        });
+//
+//
+//        update.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Map<String, Object> customer = new HashMap<>();
+//                customer.put("fullname", fullname.getText().toString());
+//                customer.put("email", email.getText().toString());
+//                customer.put("password", password.getText().toString());
+//                customer.put("iden", iden.getText().toString());
+//
+//
+//                db.collection("customer").document(idCustomer)
+//                        .set(customer)
+//                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void aVoid) {
+//                                //Log.d("cliente", "DocumentSnapshot successfully written!");
+//                                Toast.makeText(MainActivity.this,"Cliente actualizado correctmente...",Toast.LENGTH_SHORT).show();
+//
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.w("cliente", "Error writing document", e);
+//                            }
+//                        });
+//            }
+//        });
+//
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertDialogBuilder.setMessage("Está seguro de eliminar el cliente con id "+email.getText().toString()+"?");
+                alertDialogBuilder.setPositiveButton("Sí",
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                myStore.collection("seller").document(idSeller)
+                                        .delete()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(getApplicationContext(),"Cliente eliminado correctamente...",Toast.LENGTH_SHORT).show();
+                                                name.setText("");
+                                                email.setText("");
+                                                phone.setText("");
+
+                                                email.requestFocus();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w("cliente", "Error deleting document", e);
+                                            }
+                                        });
+                            }
+                        });
+
+                alertDialogBuilder.setNegativeButton("No",
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+
+
+        });
+
+
+
+
+
     }
+
+    private void saveSeller(String sName, String sEmail, String sPhone) {
+        myStore.collection("seller")
+                .whereEqualTo("email", sEmail)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (task.getResult().isEmpty()) {
+
+
+                                Map<String, Object> customer = new HashMap<>();
+                                customer.put("email", sEmail);
+                                customer.put("name", sName);
+                                customer.put("phone", sPhone);
+
+                                myStore.collection("seller")
+                                        .add(customer)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+                                                //Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                                Toast.makeText(getApplicationContext(), "Seller added sucessufuly...", Toast.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                //Log.w(TAG, "Error adding document", e);
+                                                Toast.makeText(getApplicationContext(), "Error adding customer, please check if the internet conection is stable or if the service is running correctly", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"This customer already exist",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/myshop/sale.java
+++ app/src/main/java/com/example/myshop/sale.java
@@ -1,18 +1,178 @@
 package com.example.myshop;
 
+import static java.lang.Integer.parseInt;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class sale extends AppCompatActivity {
-
+    FirebaseFirestore myStore = FirebaseFirestore.getInstance();
+    String idSeller;
+    String sellerEmail;
+    String name;
+    String phone;
+    String comision2;
+    String totalComision;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sale);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        EditText date = findViewById(R.id.etdate);
+        EditText email = findViewById(R.id.etemail);
+        EditText sales = findViewById(R.id.etsalevalue);
+        Button add = findViewById(R.id.btnadd);
+        Button search = findViewById(R.id.btnsearch);
+        Button sellersView = findViewById(R.id.btnsellers);
+
+
+
+        sellersView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSale(date.getText().toString(),email.getText().toString(),sales.getText().toString());
+
+                myStore.collection("seller")
+                        .whereEqualTo("email", email.getText().toString())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (!task.getResult().isEmpty()) {
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            idSeller = document.getId();
+                                            sellerEmail = document.getString("email");
+                                            name = document.getString("name");
+                                            phone = document.getString("phone");
+                                            comision2 = document.getString("comision");
+
+                                            Toast.makeText(sale.this,"1",Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }
+                                }
+                            }
+                        });
+
+            }
+        });
+
+
+    }
+
+    private void saveSale(String sDate, String sEmail, String sSales) {
+        myStore.collection("sales")
+                .whereEqualTo("email", sEmail)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(parseInt(sSales) >= 10000000) {
+                            if (task.isSuccessful()) {
+                                    Map<String, Object> sales = new HashMap<>();
+                                    sales.put("email", sEmail);
+                                    sales.put("date", sDate);
+                                    sales.put("salevalue", sSales);
+
+                                    myStore.collection("sales")
+                                            .add(sales)
+                                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                @Override
+                                                public void onSuccess(DocumentReference documentReference) {
+                                                    //Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                                    Toast.makeText(getApplicationContext(), "Sale added sucessufuly...", Toast.LENGTH_SHORT).show();
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    //Log.w(TAG, "Error adding document", e);
+                                                    Toast.makeText(getApplicationContext(), "Error adding customer, please check if the internet conection is stable or if the service is running correctly", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
+                                    String comision = String.valueOf(parseInt(sSales)*2/100
+                                    );
+
+                                    Map<String, Object> seller = new HashMap<>();
+                                    seller.put("email", sellerEmail);
+                                    seller.put("name", name);
+                                    seller.put("phone", phone);
+                                    seller.put("comision", comision);
+
+
+
+                                    myStore.collection("seller").document(idSeller)
+                                            .set(seller)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    //Log.d("cliente", "DocumentSnapshot successfully written!");
+                                                    Toast.makeText(sale.this,"Has conseguido una comision :D...",Toast.LENGTH_SHORT).show();
+                                                    Intent comision1 = new Intent();
+                                                    comision1.putExtra("comision", comision);
+                                                    startActivity(comision1);
+
+
+
+
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Log.w("cliente", "Error writing document", e);
+                                                }
+                                            });
+
+                                }else{
+                                Toast.makeText(getApplicationContext(),"Error ading sale",Toast.LENGTH_SHORT).show();
+                            }
+
+
+
+
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"The sale must be equal or higher than 10000000",Toast.LENGTH_SHORT).show();
+
+
+                            }
+
+
+
+
+                        }
+
+                });
     }
 }
